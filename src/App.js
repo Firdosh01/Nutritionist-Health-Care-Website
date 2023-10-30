aa6555a01e6438e70b49f048324116b79acffe7b
Firdosh01 <firdosh7392@gmail.com>
2023-10-30 01:14:18 -0500

    new project: Nutritionist Health Care Website

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>Nutritionist Health Care Website</h1>
+    </div>
+  );
+}
+
+export default App;
