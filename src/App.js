fd5ba50c2da3e68717e623ba1d72835fd2e77ab8
Firdosh01 <firdosh7392@gmail.com>
2023-10-30 22:50:33 -0500

    implement Routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/common/Navbar';
+import { Route, Routes } from 'react-router-dom';
+import Home from './components/Pages/Home';
+import About from './components/Pages/About';
+import Team from './components/Pages/Team';
+import Process from './components/Pages/Process';
+import Pricing from './components/Pages/Pricing';
+import Blog from './components/Pages/Blog';
 
 function App() {
   return (
     <div className='bg-[#EEEEEE] w-screen min-h-screen'>
       <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='about' element={<About />} />
+        <Route path='team' element={<Team />} />
+        <Route path='process' element={<Process />} />
+        <Route path='pricing' element={<Pricing />} />
+        <Route path='blog' element={<Blog />} />
+      </Routes>
+
     </div>
   );
 }
---
+++ src/components/Pages/Process.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Process() {
+  return (
+    <div>Process</div>
+  )
+}
