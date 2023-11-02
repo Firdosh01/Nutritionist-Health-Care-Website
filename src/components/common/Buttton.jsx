80aad619e1dd0fe10beca20b3fb0b2c06e2ff04e
Firdosh01 <firdosh7392@gmail.com>
2023-11-02 02:56:45 -0500

    button updated

--- src/components/common/Buttton.jsx
+++ src/components/common/Buttton.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Button({children,active}) {
   return (
     <div>
-        <button className={` ${active ? "bg-[#CBEA7B]  font-semibold py-2 px-5 rounded text-[#000] w-full" : "text-[#000] border border-[#CBEA7B] rounded py-2 px-5 font-semibold"}`}>
+        <button className={` ${active ? "bg-[#CBEA7B]  font-semibold md:py-2 md:px-5 px-2 py-1 rounded text-[#000] w-full" : "text-[#000] border border-[#CBEA7B] rounded md:py-2 md:px-5 px-2 py-1 font-semibold"}`}>
             {children}
         </button>
     </div>
